a735e3239f4aa4d95c1083992cc4a58cc98bd05f
sayheysob <sunanadjiyulian@gmail.com>
2025-04-21 17:29:29 +0700

    Create anggota.js

---
+++ anggota.js
@@ -0,0 +1,39 @@
+const dataAnggota = [
+  {
+    nama: "Nansu S Harmony",
+    posisi: "Driver",
+    status: "Training",
+    duty: "Off Duty",
+    aktivitas: [
+      { tanggal: "30/03/2025", total: "1h 37m", waktu: "30/03/25 17:22 - 30/03/25 19:00" },
+      { tanggal: "28/03/2025", total: "0h 13m", waktu: "29/03/25 00:00 - 29/03/25 00:13" }
+    ]
+  },
+  {
+    nama: "Dewi Rahayu",
+    posisi: "Petugas",
+    status: "Aktif",
+    duty: "On Duty",
+    aktivitas: []
+  },
+  {
+    nama: "Budi Santoso",
+    posisi: "Staff",
+    status: "Nonaktif",
+    duty: "Resign",
+    aktivitas: []
+  },
+  {
+    nama: "Dewi Rahayu",
+    posisi: "Petugas",
+    status: "Aktif",
+    duty: "On Duty",
+    aktivitas: []
+  },
+   {
+    nama: "Putu Tuu",
+    posisi: "Petugas",
+    status: "Aktif",
+    duty: "On Duty",
+    aktivitas: []
+  },
