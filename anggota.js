1d015b9788b2f1404682c5ee2ba2c384eb010d97
sayheysob <sunanadjiyulian@gmail.com>
2025-04-21 17:35:41 +0700

    Update anggota.js

--- anggota.js
+++ anggota.js
@@ -4,9 +4,19 @@ const dataAnggota = [
     posisi: "Driver",
     status: "Training",
     duty: "Off Duty",
+    totalJam: "1h 50m",
+    rataRataJam: "0h 55m",
     aktivitas: [
-      { tanggal: "30/03/2025", total: "1h 37m", waktu: "30/03/25 17:22 - 30/03/25 19:00" },
-      { tanggal: "28/03/2025", total: "0h 13m", waktu: "29/03/25 00:00 - 29/03/25 00:13" }
+      {
+        tanggal: "30/03/2025",
+        total: "1h 37m",
+        waktu: "30/03/25 17:22 - 30/03/25 19:00"
+      },
+      {
+        tanggal: "28/03/2025",
+        total: "0h 13m",
+        waktu: "29/03/25 00:00 - 29/03/25 00:13"
+      }
     ]
   },
   {
@@ -14,6 +24,8 @@ const dataAnggota = [
     posisi: "Petugas",
     status: "Aktif",
     duty: "On Duty",
+    totalJam: "5h 0m",
+    rataRataJam: "2h 30m",
     aktivitas: []
   },
   {
@@ -21,19 +33,8 @@ const dataAnggota = [
     posisi: "Staff",
     status: "Nonaktif",
     duty: "Resign",
+    totalJam: "0h 0m",
+    rataRataJam: "0h 0m",
     aktivitas: []
-  },
-  {
-    nama: "Dewi Rahayu",
-    posisi: "Petugas",
-    status: "Aktif",
-    duty: "On Duty",
-    aktivitas: []
-  },
-   {
-    nama: "Putu Tuu",
-    posisi: "Petugas",
-    status: "Aktif",
-    duty: "On Duty",
-    aktivitas: []
-  },
+  }
+];
